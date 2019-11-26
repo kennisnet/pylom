e4221df2f32f215a34d09d9b2123452863dc471e
Wim Muskee <wimmuskee@gmail.com>
2019-11-26 17:12:17 +0100

    update setup.py for pypi, add 3.7 to travis

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import pylom
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 setup(
     name = pylom.__name__,
@@ -15,6 +15,15 @@ setup(
     keywords = "lom parsing xml",
     packages=find_packages(),
     long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kennisnet/pylom",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries"
+    ],
     install_requires = [
          'lxml'
          ]
